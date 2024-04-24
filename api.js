f87fa2b3e29ebaa5c1137b3a7f16d87ab1454e6f
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-04-24 23:06:59 +0000

    updates

---
+++ api.js
@@ -0,0 +1,33 @@
+function getEstadoFromURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('estado');
+  }
+  
+  function updatePageTitleAndHeader() {
+    const estado = getEstadoFromURL();
+    if (estado) {
+      document.title = `Página do ${estado}`;
+      const h3Title = document.querySelector('#htres');
+      if (h3Title) {
+        h3Title.textContent = `Informações sobre ${estado}`;
+      }
+    }
+  }  
+  
+  function fetchPokemonData() {
+    const estado = getEstadoFromURL();
+    if (estado) {
+      const apiUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/${estado}`;
+      fetch(apiUrl)
+        .then(response => response.json())
+    
+        .catch(error => console.error('Erro ao obter imagem do Pokémon:', error));
+    }
+  }
+  
+  function main() {
+    updatePageTitleAndHeader();
+    fetchPokemonData();
+  }
+  
+  window.onload = main;
