52aa375e8447b12661bd4147921672078c524327
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-04-24 23:15:32 +0000

    updates

--- api.js
+++ api.js
@@ -1,33 +1,32 @@
 function getEstadoFromURL() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('estado');
-  }
-  
-  function updatePageTitleAndHeader() {
-    const estado = getEstadoFromURL();
-    if (estado) {
-      document.title = `Página do ${estado}`;
-      const h3Title = document.querySelector('#htres');
-      if (h3Title) {
-        h3Title.textContent = `Informações sobre ${estado}`;
-      }
-    }
-  }  
-  
-  function fetchPokemonData() {
-    const estado = getEstadoFromURL();
-    if (estado) {
-      const apiUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/${estado}`;
-      fetch(apiUrl)
-        .then(response => response.json())
-    
-        .catch(error => console.error('Erro ao obter imagem do Pokémon:', error));
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('estado');
+}
+
+function updatePageTitleAndHeader() {
+  const estado = getEstadoFromURL();
+  if (estado) {
+    document.title = `Página do ${estado}`;
+    const h3Title = document.querySelector('#htres');
+    if (h3Title) {
+      h3Title.textContent = `Informações sobre ${estado}`;
     }
   }
-  
-  function main() {
-    updatePageTitleAndHeader();
-    fetchPokemonData();
+}
+
+function fetchPokemonData() {
+  const estado = getEstadoFromURL();
+  if (estado) {
+    const apiUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}`;
+    fetch(apiUrl)
+      .then(response => response.json())
+      .catch(error => console.error('Erro ao obter imagem do Pokémon:', error));
   }
-  
-  window.onload = main;
+}
+
+function main() {
+  updatePageTitleAndHeader();
+  fetchPokemonData();
+}
+
+window.onload = main;
