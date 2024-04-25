d094d2b4dc1f081005d650ce5cf4459ab5d17857
ashuraa <91293924+ashura-senpai@users.noreply.github.com>
2024-04-25 00:44:21 +0000

    updates

--- api.js
+++ api.js
@@ -9,10 +9,37 @@ function updatePageTitleAndHeader() {
     document.title = `Página do ${nome}`;
     const h3Title = document.querySelector('#htres');
     if (h3Title) {
-      h3Title.textContent = `Informações sobre ${nome}`;
+      h3Title.textContent = `${nome}`;
     }
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const favForm = document.getElementById('favorito-form');
+  const favList = document.getElementById('todo-list');
+
+  function loadTodoList() {
+    const favs = JSON.parse(localStorage.getItem('favs')) || [];
+    favList.innerHTML = '';
+    favs.forEach(todo => {
+      const li = document.createElement('li');
+      li.textContent = todo;
+      favList.appendChild(li);
+    });
+  }
+
+  loadTodoList();
+
+  favForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (favForm) {
+      const favs = JSON.parse(localStorage.getItem('favs')) || [];
+      favs.push(favForm);
+      localStorage.setItem('favs', JSON.stringify(favs));
+      loadTodoList();
+    }
+  });
+});
 
 function main() {
   updatePageTitleAndHeader();
